5d231ceef2c6f748a0fdd34df2bf36c8cc2df8e6
adithwip <adithwidyapradipta@gmail.com>
2019-08-02 18:35:11 +0700

    add gatsby transformer for markdown files to be blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
