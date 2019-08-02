5dd60e7e90af921f162e0e57a6c90ff4a5499b08
adithwip <adithwidyapradipta@gmail.com>
2019-08-02 16:57:59 +0700

    useStaticQuery to header component which is not a pages component to retrieve siteMetaData

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,49 @@
 import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import styled from 'styled-components'
 
 import Container from '../layouts/Container'
 import Item from '../layouts/Item'
 
-import { Link } from 'gatsby'
-
 const StyledHeader = styled.header`
   padding: 16px;
 `
 
-const Header = () => (
-  <StyledHeader className="header">
-    <Container flexDirection="column" alignItems="center">
-      <Item align="center">
-        <h1>Adith Widya Pradipta</h1>
-      </Item>
-      <Item>
-        <Container spacing={16}>
-          <Item>
-            <Link to="/">Home</Link>
-          </Item>
-          <Item>
-            <Link to="/about">About</Link>
-          </Item>
-          <Item>
-            <Link to="/blog">Blog</Link>
-          </Item>
-        </Container>
-      </Item>
-    </Container>
-  </StyledHeader>
-)
+const Header = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <StyledHeader id="header">
+      <Container flexDirection="column" alignItems="center">
+        <Item align="center">
+          <h1>{data.site.siteMetadata.title}</h1>
+        </Item>
+        <Item>
+          <Container spacing={16}>
+            <Item>
+              <Link to="/">Home</Link>
+            </Item>
+            <Item>
+              <Link to="/about">About</Link>
+            </Item>
+            <Item>
+              <Link to="/blog">Blog</Link>
+            </Item>
+          </Container>
+        </Item>
+      </Container>
+    </StyledHeader>
+  )
+}
 
 export default Header
