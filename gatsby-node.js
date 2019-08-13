9c4ca47328ffd4d91454fa02ad88367d3f0f1c7d
adithwip <adithwidyapradipta@gmail.com>
2019-08-13 18:32:55 +0700

    createPages based on contentful graphQL slug, instead of markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,45 +2,99 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node)
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions
+//   fmImagesToRelative(node)
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `posts` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const slug = createFilePath({ node, getNode, basePath: `posts` })
+//     createNodeField({
+//       node,
+//       name: `slug`,
+//       value: slug,
+//     })
+//   }
+// }
 
-exports.createPages = ({ graphql, actions }) => {
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//       createPage({
+//         path: node.fields.slug,
+//         component: path.resolve(`./src/templates/Post.js`),
+//         context: {
+//           // Data passed to context is available
+//           // in page queries as GraphQL variables.
+//           slug: node.fields.slug,
+//         },
+//       })
+//     })
+//   })
+// }
+
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const result = await graphql(`
+//     query {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: node.fields.slug,
+//       component: path.resolve(`./src/templates/Post.js`),
+//       context: {
+//         slug: node.fields.slug,
+//       }
+//     })
+//   })
+// }
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  return graphql(`
-    {
-      allMarkdownRemark {
+  const result = await graphql(`
+    query {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
-  `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.fields.slug,
-        component: path.resolve(`./src/templates/Post.js`),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.fields.slug,
-        },
-      })
+  `)
+
+  console.log('result', result)
+
+  result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
+    createPage({
+      path: node.slug,
+      component: path.resolve(`./src/templates/Post.js`),
+      context: {
+        slug: node.slug
+      }
     })
   })
 }
