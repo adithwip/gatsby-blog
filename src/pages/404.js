485753799d97705f990a6581445eaf6cf46efc4b
adithwip <adithwidyapradipta@gmail.com>
2019-08-13 18:33:10 +0700

    create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../layouts/Layout"
+
+const NotFound = () => (
+  <Layout
+    siteTitle="404 Page"
+    siteDescription="The page you are looking is not found"
+  >
+    <h1>The page you are looking is not found</h1>
+  </Layout>
+)
+
+export default NotFound
