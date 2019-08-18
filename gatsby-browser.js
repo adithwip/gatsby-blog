6b68db41a6224202725ac3616c9856d08a4de3d1
adithwip <adithwidyapradipta@gmail.com>
2019-08-18 09:20:11 +0700

    put prismjs and other css to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
-// require('prismjs/themes/prism-tomorrow.css')
-// require('prismjs/plugins/line-numbers/prism-line-numbers.css')
+require('./src/styles/index.css')
+require('prismjs/themes/prism-tomorrow.css')
+require('prismjs/plugins/line-numbers/prism-line-numbers.css')
+require('./src/styles/prism-override.css')
