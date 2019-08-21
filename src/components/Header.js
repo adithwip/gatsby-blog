530a15d7d3aef9bd44a4330942be514f20290069
adithwip <adithwidyapradipta@gmail.com>
2019-08-21 13:14:57 +0700

    add headroom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
+import Headroom from "react-headroom"
 
 import Container from "../layouts/Container"
 import Item from "../layouts/Item"
 
-const StyledHeader = styled.header`
+const StyledHeader = styled(Headroom)`
   padding: 16px;
+
+  & .headroom--scrolled {
+    background-color: white;
+  }
 `
 
 const Header = () => {
@@ -23,26 +28,28 @@ const Header = () => {
   )
 
   return (
-    <StyledHeader id="header">
+    <header>
       <Container flexDirection="column" alignItems="center">
         <Item align="center">
           <h1>{data.site.siteMetadata.title}</h1>
         </Item>
-        <Item>
-          <Container spacing={16}>
-            <Item>
-              <Link to="/">Home</Link>
-            </Item>
-            <Item>
-              <Link to="/about">About</Link>
-            </Item>
-            <Item>
-              <Link to="/blog">Blog</Link>
-            </Item>
-          </Container>
-        </Item>
+        <StyledHeader>
+          <Item align="center">
+            <Container spacing={16} justify="center">
+              <Item>
+                <Link to="/">Home</Link>
+              </Item>
+              <Item>
+                <Link to="/about">About</Link>
+              </Item>
+              <Item>
+                <Link to="/blog">Blog</Link>
+              </Item>
+            </Container>
+          </Item>
+        </StyledHeader>
       </Container>
-    </StyledHeader>
+    </header>
   )
 }
 
