5ea02256464c46b975f2306cb3d0a3c4fd2b7df7
adithwip <adithwidyapradipta@gmail.com>
2019-08-23 14:22:19 +0700

    override typography theme styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,10 @@
 import Typography from "typography"
 import kirkhamTheme from "typography-theme-kirkham"
+kirkhamTheme.overrideThemeStyles = () => ({
+  a: {
+    color: "#355834",
+  },
+})
 
 const typography = new Typography(kirkhamTheme)
 
