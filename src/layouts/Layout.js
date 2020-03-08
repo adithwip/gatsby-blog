60b2d1de54c7822008c133c72d8424af1b0bba44
adithwip <adithwidyapradipta@gmail.com>
2020-03-08 08:55:20 +0700

    update ver gatsby, react, react-dom, darkmode-js

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
-// import Darkmode from "darkmode-js"
+import Darkmode from "darkmode-js"
 
 import SectionContainer from "./SectionContainer"
 import Container from "./Container"
